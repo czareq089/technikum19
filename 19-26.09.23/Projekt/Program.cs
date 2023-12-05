22fcbe0932a2d90148af8ad23958f3cec58ada24
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-12-05 08:04:37 +0100

    dodanie folderu

---
+++ 19-26.09.23/Projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+sbyte 	    -128                   -                  127                                (  8  bitów => 1 bajt   )
+byte 	      0                    -                  255 	                             (  8  bitów => 1 bajt   )
+short 	   -32,768                 -                 32 767 	                         (  16 bitów => 2 bajty  )
+ushort 	      0                    -                 65 535                              (  16 bitów => 2 bajty  )
+int 	-2,147,483,648             -              2 147 483 647 	                     (  32 bity =>  4 bajty  )
+uint 	      0                    -              4 294 967 295 	                     (  32 bity =>  4 bajty  )
+long 	-9,223,372,036,854,775,808 -        9 223 372 036 854 775 807 	                 (  64 bity =>  8 bajtów )
+ulong 	      0                    -        18 446 744 073 709 551 615 	                 (  64 bity =>  8 bajtów )
+bool         true                  -                 false
+char        U+0000                 -                U-FFFF
+float       -3.4 x 10^38           -              3.4 x 10^38                            (  32 bity   )
+double                                                                                   (  64 bity   )
+decimal                                                                                  (  128 bitów )
+*/
