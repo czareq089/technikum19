23d0ebef316fd632df8fa3ad613c1676e3c7a2b9
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2024-02-06 09:23:11 +0100

    dodanie lekcji 06.02

---
+++ pob/Konstruktory 06.02/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Example example = new Example("Hello World");
+        example.PrintMessage(example.Message);
+    }
+}
+
+//Przykład
+class Example
+{
+    public string Message { get; set; }
+    
+    //przykład konstruktora
+    public Example(string message)
+    {
+        Message = message;
+    }
+    
+    //przykład metody
+   public void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
