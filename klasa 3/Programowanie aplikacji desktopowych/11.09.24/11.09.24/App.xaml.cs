e8ec6fecfdf0bce0e0d0cb7e67538736bd2301b1
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2024-09-11 12:40:16 +0200

    dodanie pad 11.09.24

---
+++ klasa 3/Programowanie aplikacji desktopowych/11.09.24/11.09.24/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _11._09._24;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    
+}
+
