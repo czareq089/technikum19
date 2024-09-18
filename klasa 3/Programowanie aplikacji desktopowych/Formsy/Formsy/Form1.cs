49b469692e36d1a98e90a951aceab5a326695001
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2024-09-18 12:41:59 +0200

    dodanie zadań

---
+++ klasa 3/Programowanie aplikacji desktopowych/Formsy/Formsy/Form1.cs
@@ -0,0 +1,10 @@
+using System.Windows.Forms;
+namespace Formsy;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ klasa 3/Programowanie aplikacji desktopowych/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            textBox1.AppendText($"{DateTime.Now.ToLongDateString()}\r\n");
+        }
+    }
+}
