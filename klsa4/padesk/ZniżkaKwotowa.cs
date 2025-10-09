efd6868fae457dcf35733b5167bfbf8c56ddde80
Cezary Kotlarski <86021893+czareq089@users.noreply.github.com>
2025-10-09 14:03:02 +0200

    Dodanie plików

---
+++ klsa4/padesk/ZniżkaKwotowa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class ZniżkaKwotowa : IZniżkaStrategia
+    {
+        public int obliczZniżke(int kwota, int x)
+        {
+            return kwota - x;
+        }
+    }
+}
---
+++ klsa4/padesk/ZniżkaProcentowa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class ZniżkaProcentowa : IZniżkaStrategia
+    {
+        public int obliczZniżke(int kwota, int x)
+        {
+            return (kwota * (100 - x)) / 100;
+        }
+    }
+}
