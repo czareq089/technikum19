1dd2696e622ad46cf53c123eb09aa3b0f1984b37
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2025-11-26 19:32:37 +0100

    jakis force push

--- klasa 4/pob/Restaurant/Restaurant/obj/Debug/net9.0/Restaurant.AssemblyInfo.cs
+++ klasa 4/pob/Restaurant/Restaurant/obj/Debug/net9.0/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951b2d2d8c07a0cc565bed2141a70744d7e387e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
